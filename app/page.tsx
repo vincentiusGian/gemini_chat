c1d93f4e894ff0baba12a30c873164cbc3eccd5b
vincentiusgian <gianjunius54@gmail.com>
2024-07-07 23:51:54 +0700

    selesaiiii

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,13 @@ const {messages, input, handleInputChange, handleSubmit, isLoading, stop} = useC
   function RenderMessages(){
     return <div className="flex flex-col-reverse w-full text-left mt-4 gap-4 whitespace-pre-wrap"> 
         {messages.map((m, index)=> {
-          return <div className={`p-4 shadow-md rounded-md ml-10 relative ${m.role === 'user' ? "bg-stone-300": ""}`}>
+          return (<div className={`p-4 shadow-md rounded-md ml-10 relative ${m.role === 'user' ? "bg-stone-300": ""}`}>
             <Markdown text={m.content}/>
             {m.role === 'user' ? <User2 className="absolute top-2 -left-10 border rounded-full p-1 shadow-lg"/> : 
             <Bot className={`absolute top-2 -left-10 border rounded-full p-1 shadow-lg ${
               isLoading && index-1 === messages.length-1 ? 'animate-bounce' : ""
             }`}/>}
-          </div>
+          </div>);
         })}
     </div>
   }
